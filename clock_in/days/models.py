02ae922ebe2ffbb8384aae87d5718f65f4ff9ab7
Álvaro Calvo García <cokeiv@gmail.com>
2020-03-19 23:34:00 +0100

    Add properties to Month and WorkDay models

--- clock_in/days/models.py
+++ clock_in/days/models.py
@@ -31,6 +31,41 @@ class Month(models.Model):
     year = models.ForeignKey(Year, on_delete=models.CASCADE, related_name='months')
 
     unique_together = ['month', 'year']
+    
+    @property
+    def user(self) -> User:
+        return self.year.user
+    
+    @property
+    def time_worked(self) -> datetime.timedelta:
+        time_worked = datetime.timedelta(0)
+        for day in self.worked_days.all():
+            time_worked += day.time_worked
+        return time_worked
+    
+    @property
+    def working_time_delta(self) -> datetime.timedelta:
+        day_working_time = self.user.day_working_time
+        day_working_time_delta = datetime.timedelta(
+            hours=day_working_time.hour,
+            minutes=day_working_time.minute,
+            seconds=day_working_time.second)
+        return self.worked_days.count() * day_working_time_delta
+    
+    @property
+    def time_balance(self) -> datetime.timedelta:
+        time_balance = datetime.timedelta(0)
+        for day in self.worked_days.all():
+            time_balance += day.time_balance
+        return time_balance
+
+    @property
+    def time_balance_positive(self) -> bool:
+        return self.time_balance >= datetime.timedelta(0)
+
+    @classmethod
+    def get_month_url(cls, month, year):
+        return reverse("days:month", kwargs={"month": month, "year": year})
 
     def get_absolute_url(self):
         return reverse("days:month", kwargs={"month": self.month, "year": self.year.year})
@@ -50,6 +85,10 @@ class WorkDay(models.Model):
     unique_together = ['day', 'month']
 
     @property
+    def user(self) -> User:
+        return self.month.year.user
+
+    @property
     def time_worked(self) -> datetime.timedelta:
         if not self.start_time or not self.end_time:
             raise ValueError("Both start_time and end_time must be set in order to calculate the worked time")
@@ -66,5 +105,33 @@ class WorkDay(models.Model):
 
         return time_elapsed
 
+    @property
+    def working_time_delta(self) -> datetime.timedelta:
+        day_working_time = self.user.day_working_time
+        return datetime.timedelta(
+            hours=day_working_time.hour,
+            minutes=day_working_time.minute,
+            seconds=day_working_time.second)
+
+    @property
+    def time_balance(self) -> datetime.timedelta:
+        courtesy_time = datetime.timedelta(0)
+        user = self.user
+        start_time_limit = datetime.time(hour=10) if user.time_flexibility else datetime.time(hour=9)
+        end_time_limit = datetime.time(hour=14)
+        if self.start_time < start_time_limit:
+            courtesy_time += datetime.timedelta(minutes=10)
+        if self.end_time > end_time_limit:
+            courtesy_time += datetime.timedelta(minutes=10)
+        return self.time_worked + courtesy_time - self.working_time_delta
+    
+    @property
+    def time_balance_positive(self) -> bool:
+        return self.time_balance >= datetime.timedelta(0)
+
+    @classmethod
+    def get_day_url(cls, day: int, month:int , year: int):
+        return reverse("days:day", kwargs={"day": day, "month": month, "year": year})
+
     def get_absolute_url(self):
-        return reverse("days:year", kwargs={"year": self.month.year.year})
+        return reverse("days:day", kwargs={"day": self.day, "month": self.month.month, "year": self.month.year.year})
