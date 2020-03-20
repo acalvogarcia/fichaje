eccc6e2fc9e97c529a16a2bc43882fcad7e4bd60
Álvaro Calvo García <cokeiv@gmail.com>
2020-03-20 18:58:59 +0100

    Create new view for clock_in entrypoint

--- clock_in/days/urls.py
+++ clock_in/days/urls.py
@@ -4,6 +4,7 @@ from .views import DayDetailView, MonthDetailView, YearDetailView
 
 app_name = "days"
 urlpatterns = [
+    path("", DayDetailView.as_view(), name="day-today"),
     path("<int:year>", YearDetailView.as_view(), name="year"),
     path("<int:year>/<int:month>", MonthDetailView.as_view(), name="month"),
     path("<int:year>/<int:month>/<int:day>", DayDetailView.as_view(), name="day"),
