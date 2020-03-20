4c158813302d4754b9e3b8b4975d4e3448f20dbc
Álvaro Calvo García <cokeiv@gmail.com>
2020-03-20 17:42:23 +0100

    Change app name error

--- clock_in/days/urls.py
+++ clock_in/days/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import DayDetailView, MonthDetailView, YearDetailView
 
-app_name = "users"
+app_name = "days"
 urlpatterns = [
     path("<int:year>", YearDetailView.as_view(), name="year"),
     path("<int:year>/<int:month>", MonthDetailView.as_view(), name="month"),
